928ebc56cb1ca55b7a70a96dc1d9e36cb40dc7be
Petrovvich <alexromantrue@gmail.com>
2020-06-25 13:32:59 +0700

    smoll fixes in tasks

--- all_code_examples/optimizing-for-throughput-example/src/main/java/ru/petrovich/optimizing/throughput/ThroughputHttpServer.java
+++ all_code_examples/optimizing-for-throughput-example/src/main/java/ru/petrovich/optimizing/throughput/ThroughputHttpServer.java
@@ -1,26 +1,4 @@
-package ru.petrovich.optimizing.throughput;/*
- * MIT License
- *
- * Copyright (c) 2019 Michael Pogrebinsky
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+package ru.petrovich.optimizing.throughput;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -39,8 +17,8 @@ import java.util.concurrent.Executors;
  * https://www.udemy.com/java-multithreading-concurrency-performance-optimization
  */
 public class ThroughputHttpServer {
-    private static final String INPUT_FILE = "./resources/war_and_peace.txt";
-    private static final int NUMBER_OF_THREADS = 8;
+    private static final String INPUT_FILE = "/projects/personal/personal/udemy-java-multithreading-concurrency/all_code_examples/optimizing-for-throughput-example/src/main/resources/war_and_peace.txt";
+    private static final int NUMBER_OF_THREADS = 12;
 
     public static void main(String[] args) throws IOException {
         String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
